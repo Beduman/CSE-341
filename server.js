84ad589e3ebd2e0fa6c5df9da40c5798a28fe8dc
Beduman <behunz77@gmail.com>
2025-05-10 20:46:26 -0700

    lesson 1 learning

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const lesson1Controller = require('./controllers/lesson1');
 const app = express();
 
- 
-app.get('/', lesson1Controller.testRoute);
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+
+app.use('/', require('./routes'));
+
+const port = 8080
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
